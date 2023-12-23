b9d6e6ca187202904e1947dde06c9e4049d00d7f
Naeem Navjivan <n.navjivan@qvitec.in>
2023-12-23 18:33:14 +0530

    toploader add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import NextTopLoader from 'nextjs-toploader';
 import './globals.css'
 import {Providers} from './providers'
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <NextTopLoader />
       <Providers>
         {children}
       </Providers>

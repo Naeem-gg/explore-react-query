fb4e335a0a862a12ec17ba9fc5197a0ff90a1ad9
Naeem Navjivan <n.navjivan@qvitec.in>
2023-12-25 19:02:43 +0530

    home 2 selected

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,30 @@
-import Link from "next/link";
-import React from "react";
+import React from 'react'
+import SearchableList from '@/app/components/searching'
+import { Amiri } from 'next/font/google'
+import prisma from '@/prisma/client'
+const inter = Amiri({subsets:['arabic'],weight:"400" })
 
-const page = () => {
+const getData = async()=>{
+  try {
+    
+    const res = await prisma.time.findMany({})
+    if(res !== undefined)return res
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const page = async() => {
+
+  const data = await getData()
+  if(data === undefined)return
+  console.log(data)
   return (
-    <div className="h-screen w-screen flex justify-around items-center">
-      <Link
-        href="/home1"
-        className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
-      >
-        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-          Home 1
-        </span>
-      </Link>
-      <Link
-        href="/home2"
-        className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
-      >
-        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-          Home 2
-        </span>
-      </Link>
+    <div className='h-screen w-screen'>
+      <div className={`${inter.className} w-screen h-1/6 text-center p-4 text-3xl`}>إِنَّ الصَّلَاةَ تَنْهَىٰ عَنِ الْفَحْشَاءِ وَالْمُنكَرِ </div>
+      <SearchableList data={data}/>
     </div>
-  );
-};
+  )
+}
 
-export default page;
+export default page
